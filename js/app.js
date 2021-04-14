a9f586961a9732b0b38d2e3ef162a1fdb385f5f4
Tino Sabetta <tino.sabetta@gmail.com>
2021-04-14 20:09:38 +0200

    added a basic hash based router to load external content

--- js/app.js
+++ js/app.js
@@ -2,14 +2,39 @@ var app = (function() {
     /* --------------------------------------------------------------------------------------------------
     Variables
     ---------------------------------------------------------------------------------------------------*/
-
+    var startingPageName = "home";
+    var fileType = "html";
+    var directory = "content";
+    var contentElement = document.querySelector("main");
 
     /* --------------------------------------------------------------------------------------------------
     functions
     ---------------------------------------------------------------------------------------------------*/
+    function router() {
+      var hash = location.hash.substring(1) || startingPageName;
+
+      fetch(directory+"/"+hash+"."+fileType)
+        .then(function(response) {
+          if (!response.ok) {
+            throw new Error("HTTP error, status = " + response.status);
+          }
+          return response.text();
+        })
+        .then(function(text) {
+          contentElement.innerHTML = text;
+        })
+        .catch(function(error) {
+          contentElement.innerHTML = '';
+          contentElement.appendChild(
+            document.createTextNode('Error: ' + error.message)
+          );
+        });
+    }
 
     function init() {
         document.addEventListener("touchstart", function() {}, false);
+        window.addEventListener("hashchange", router, false);
+        window.addEventListener("DOMContentLoaded", router, false);
     }
 
     /* --------------------------------------------------------------------------------------------------
